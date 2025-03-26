65babb59aa28e66838006f856aa8cb2c64a0b77c
Leon <166517099+scuranger0625@users.noreply.github.com>
2025-03-26 21:32:56 +0800

    Add files via upload

--- QuickfindUF.java
+++ QuickfindUF.java
@@ -9,5 +9,19 @@ public class QuickfindUF {
             id[i] = i; // id[i]是一個陣列
         }
     }
-    
+// boolean connected 是一個布林值   
+public boolean connected(int p, int q) {
+    return id[p] == id[q];
 }
+
+// void union 是一個空值
+public void union(int p, int q) {
+    int pid = id[p];
+    int qid = id[q];
+    // 這個迴圈是把每個元素都設定為自己 id.length是一個長度 i++是一個遞增
+    for (int i = 0; i < id.length; i++) 
+        // 如果 id[i] 等於 pid 就把 id[i] 設定為 qid
+        if (id[i] == pid) id[i] = qid;
+        }
+    }
+
